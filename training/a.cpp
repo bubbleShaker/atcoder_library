c20dd97e1528521fdd932b019e4f862bcd181712
bubble_shaker <intselectk@gmail.com>
2024-06-13 04:04:11 +0900

    Change .vscode

--- training/a.cpp
+++ training/a.cpp
@@ -1,8 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 int main(){
-  int n;
-  cin>>n;
-  cout<<((n<=999)?"ABC":"ABD")<<'\n';
+  int a,b,c;
+  std::cin>>a>>b>>c;
+  std::cout<<21-(a+b+c)<<'\n';
   return 0;
 }
