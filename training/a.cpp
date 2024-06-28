4f4db3b1039fa7bda9bf41282873c4370b0cbe15
bubble_shaker <intselectk@gmail.com>
2024-06-29 00:51:54 +0900

    Change .vscode

--- training/a.cpp
+++ training/a.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include<iomanip>
 int main(){
-  int p;
-  std::cin>>p;
-  auto fact=[&](int n){
-    int ret=1;
-    while(n>0){
-      ret*=n--;
-    }
-    return ret;
-  };
-  std::vector<int> cnt(11,100);
-  int ans=0;
-  for(int i=10;i>=0;i--){
-    if(p==0){
-      break;
-    }
-    int c=p/fact(i);
-    p-=std::min(100,c)*fact(i);
-    ans+=std::min(100,c);
-  }
+  double sx,sy,gx,gy;
+  std::cin>>sx>>sy>>gx>>gy;
+  gy=-gy;
+  double dx=std::abs(gx-sx);
+  double dy=std::abs(gy-sy);
+  std::cout<<std::fixed<<std::setprecision(10);
+  double ans=((gx-sx>0)?sx+(dx*sy)/dy:sx-(dx*sy)/dy);
   std::cout<<ans<<'\n';
   return 0;
 }
