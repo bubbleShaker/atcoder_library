6336901689c5a8f8bdc86b39ded118b86776a023
bubble_shaker <intselectk@gmail.com>
2024-09-14 16:08:38 +0900

    something is changed

--- training/a.cpp
+++ training/a.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
 int main(){
-  int n;
-  std::cin>>n;
-  std::vector<int> a(n);
-  for(int i=0;i<n;i++){
-    std::cin>>a[i];
+  int t;
+  std::cin>>t;
+  while(t--){
+    std::string s;
+    std::cin>>s;
+    std::cout<<s[0]-'0'+s[1]-'0'<<'\n';
   }
-  std::string s;
-  std::cin>>s;
-  const int CHAR_MAX=3;
-  const int SET_MAX=1<<4; //2^4 
-  std::cout<<SET_MAX<<'\n';
   return 0;
 }
