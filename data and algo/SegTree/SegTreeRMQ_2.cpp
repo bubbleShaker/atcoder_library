48600ae1225f086c8403f5245ec1c367f0c11647
Soh <intselectk@gmail.com>
2024-11-07 00:50:30 +0900

    RMQのセグ木写経1日目

--- data and algo/SegTree/SegTreeRMQ_2.cpp
+++ data and algo/SegTree/SegTreeRMQ_2.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 参照:鉄則本p331
 
 class SegmentTree{
 public:
-  int dat[300000],siz=1;
+  int dat[300000], siz=1;
   
-  //要素datの初期化を行う(最初は全部ゼロ)
+  // 要素datの初期化を行う(最初は全部ゼロ)
   void init(int N){
     siz=1;
     while(siz<N) siz*=2;
     for(int i=1;i<siz*2;i++) dat[i]=0;
   }
   
-  //クエリ1に対する処理
+  // クエリ1に対する処理
   void update(int pos,int x){
     pos=pos+siz-1;
     dat[pos]=x;
@@ -22,15 +23,15 @@ public:
     }
   }
   
-  //クエリ2に対する処理
-  //uは現在のセル番号、[a,b)はセルに対応する半開区間、[l,r)は求めたい半開区間
+  // クエリ2に対する処理
+  // uは現在のセル番号、[a,b)はセルに対応する半開区間、[l,r)は求めたい半開区間
   int query(int l,int r,int a,int b,int u){
     if(r<=a||b<=l) return -1000000000;//一切含まれない場合
     if(l<=a&&b<=r) return dat[u];//完全に含まれる場合
     int m=(a+b)/2;
     int AnswerL=query(l,r,a,m,u*2);
     int AnswerR=query(l,r,m,b,u*2+1);
-    return max(AnswerL,AnswerR);    
+    return max(AnswerL,AnswerR);
   }
 };
 
