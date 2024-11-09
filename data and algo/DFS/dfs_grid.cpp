5f9ce61f564d512ce338eb330b23cd6d9d3d1105
Soh <intselectk@gmail.com>
2024-11-09 13:39:43 +0900

    グリッド上のdfsを暗記した

--- data and algo/DFS/dfs_grid.cpp
+++ data and algo/DFS/dfs_grid.cpp
@@ -4,21 +4,21 @@ using namespace std;
 using P=pair<int,int>;
 
 int H,W;
-vector<vector<char>> maze;
-vector<vector<bool>> seen;
+vector<vector<char>> maze; //迷路の内容を保存する配列
+vector<vector<bool>> seen; //すでに通った場所なのかを記録
 P s,g;
 
 //dfs
-int dx[4]={1,-1,0,0};
+int dx[4]={1,-1,0,0}; //dx,dyは次の探索場所への距離を表す(4方向分)
 int dy[4]={0,0,1,-1};
 void dfs(P p){
   seen[p.first][p.second]=true;
   for(int i=0;i<4;i++){
-    int ny=p.first+dy[i];
-    int nx=p.second+dx[i];
-    if(ny<0||H<=ny||nx<0||W<=nx) continue;
-    if(maze[ny][nx]=='#') continue;
-    if(seen[ny][nx]) continue;
+    int ny=p.first+dy[i]; //次の探索場所の行番号
+    int nx=p.second+dx[i]; //次の探索場所の列番号
+    if(ny<0||H<=ny||nx<0||W<=nx) continue; //迷路の外に出るならスルー
+    if(maze[ny][nx]=='#') continue; //障害物があればスルー
+    if(seen[ny][nx]) continue; //探索済みならスルー
     dfs(make_pair(ny,nx));
   }
 }
