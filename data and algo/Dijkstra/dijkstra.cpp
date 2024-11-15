03336d56a95e2b9677e210fbc14394c34e831ea6
Soh <intselectk@gmail.com>
2024-11-15 23:57:37 +0900

    ダイクストラ暗記

--- data and algo/Dijkstra/dijkstra.cpp
+++ data and algo/Dijkstra/dijkstra.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+//参考:https://algo-logic.info/dijkstra/
+
 struct Edge{
   long long to;
   long long cost;
@@ -8,19 +10,27 @@ using Graph=vector<vector<Edge>>;
 using P=pair<int,int>;
 const long long INF=1LL<<60;
 
+/* dijkstra(G,s,dis)
+    入力:グラフG,開始点s,距離を格納するdis
+    計算量:O(|E|log|V|)
+    副作用:disが書き換えられる
+*/
+
 void dijkstra(const Graph &G,int s,vector<long long> &dis){
   int N=G.size();
   dis.resize(N,INF);
+  priority_queue<P,vector<P>,greater<P>> pq; //「仮の最短距離,頂点」が小さい順に並ぶ
   dis[s]=0;
-  priority_queue<P,vector<P>,greater<P>> pq;
   pq.emplace(dis[s],s);
   while(!pq.empty()){
     P p=pq.top();
     pq.pop();
     int v=p.second;
-    if(dis[v]<p.first) continue;
+    if(dis[v]<p.first){ //最短距離でなければ無視
+      continue;
+    }
     for(auto &e:G[v]){
-      if(dis[e.to]>dis[v]+e.cost){
+      if(dis[e.to]>dis[v]+e.cost){ //最短距離候補ならpriority_queueに追加
         dis[e.to]=dis[v]+e.cost;
         pq.emplace(dis[e.to],e.to);
       }
