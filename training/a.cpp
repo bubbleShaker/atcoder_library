fc57134f198f740413df5d7473dd4a2df35e15d3
Soh <intselectk@gmail.com>
2024-11-18 17:01:16 +0900

    テスト

--- training/a.cpp
+++ training/a.cpp
@@ -1,33 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct Edge{
+  long long to;
+  long long cost;
+};
+using Graph=vector<vector<Edge>>;
+using P=pair<int,int>;
+const long long INF=1LL<<60;
 
-char flip(char c){
-  if('a'<=c&&c<='z'){
-    return (c-'a')+'A';
-  }
-  else{
-    return (c-'A')+'a';
+void dijkstra(const Graph &G,int s,vector<long long> &dis,vector<int> &prev){
+  int N=G.size();
+  dis.resize(N,INF);
+  prev.resize(N,-1);
+  dis[s]=0;
+  priority_queue<P,vector<P>,greater<P>> pq;
+  pq.emplace(dis[s],0);
+  while(!pq.empty()){
+    P p=pq.top();
+    pq.pop();
+    int v=p.second;
+    if(dis[v]<p.first){
+      continue;
+    }
+    for(auto &e:G[v]){
+      if(dis[e.to]>dis[v]+e.cost){
+        dis[e.to]=dis[v]+e.cost;
+        prev[e.to]=v;
+        pq.emplace(dis[e.to],e.to);
+      }
+    }
   }
 }
 
-int main(){
-  string s;
-  cin>>s;
-  int q;
-  cin>>q;
-  for(int i=0;i<q;i++){
-    if(i){cout<<" ";}
-    long long k;
-    cin>>k;
-    k--;
-    long long blk=k/s.size();
-    long long pt=k%s.size();
-    if(__builtin_popcountll(blk)%2){
-      cout<<flip(s[pt]);
-    }
-    else{
-      cout<<s[pt];
-    }
-  }cout<<endl;
-  return 0;
+vector<int> get_path(const vector<int> &prev,int t){
+  vector<int> path;
+  for(int cur=t;cur!=-1;cur=prev[cur]){
+    path.push_back(cur);
+  }
+  reverse(path.begin(),path.end());
+  return path;
 }
